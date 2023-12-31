20cc5a40481add0ae47354b2c09e091a114a61d4
OthmanHaba <alvondrvio@gmail.com>
2023-12-31 12:39:04 +0200

    adding the trainee mangement

---
+++ resources/js/Components/Drawer.vue
@@ -0,0 +1,153 @@
+<template>
+    <div>
+        <div class="drawer" :class="{ 'is-open': isOpen, 'is-visible': isVisible }">
+            <div
+                class="drawer__overlay"
+                :style="{ transitionDuration: `${speed}ms` }"
+            ></div>
+            <div
+                class="drawer__content"
+                v-click-away="closeDrawer"
+                :style="{
+          maxWidth: maxWidth,
+          transitionDuration: `${speed}ms`,
+          backgroundColor: backgroundColor,
+        }"
+            >
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+ import { directive } from "vue3-click-away";
+
+
+export default {
+    name: "Drawer",
+
+    directives: {
+        ClickAway: directive,
+    },
+
+    props: {
+        isOpen: {
+            type: Boolean,
+            required: false,
+            default: false,
+        },
+
+        maxWidth: {
+            type: String,
+            required: false,
+            default: "400px",
+        },
+
+        // Transition Speed in Milliseconds
+        speed: {
+            type: Number,
+            required: false,
+            default: 300,
+        },
+
+        backgroundColor: {
+            type: String,
+            required: false,
+            default: "#fafafa",
+        },
+    },
+
+    data() {
+        return {
+            isVisible: false,
+            isTransitioning: false,
+        };
+    },
+
+    watch: {
+        isOpen(val) {
+            this.isTransitioning = true;
+
+            if (val) {
+                this.toggleBackgroundScrolling(true);
+                this.isVisible = true;
+            } else {
+                this.toggleBackgroundScrolling(false);
+                setTimeout(() => (this.isVisible = false), this.speed);
+            }
+
+            setTimeout(() => (this.isTransitioning = false), this.speed);
+        },
+    },
+
+    methods: {
+        toggleBackgroundScrolling(enable) {
+            const body = document.querySelector("body");
+
+            body.style.overflow = enable ? "hidden" : null;
+        },
+
+        closeDrawer() {
+            console.log("closeDrawer");
+            if (!this.isTransitioning) {
+                this.$emit("close");
+            }
+        },
+    },
+
+    mounted() {
+        this.isVisible = this.isOpen;
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.drawer {
+    visibility: hidden;
+
+    &.is-visible {
+        visibility: visible;
+    }
+
+    &.is-open {
+        .drawer__overlay {
+            opacity: 0.5;
+        }
+
+        .drawer__content {
+            transform: translateX(0);
+        }
+    }
+
+    &__overlay {
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        z-index: 200;
+        opacity: 0;
+        transition-property: opacity;
+        background-color: #000000;
+        user-select: none;
+    }
+
+    &__content {
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        height: 100%;
+        width: 100%;
+        z-index: 9999;
+        overflow: auto;
+        transition-property: transform;
+        display: flex;
+        flex-direction: column;
+        transform: translateX(100%);
+        box-shadow: 0 2px 6px #777;
+    }
+}
+</style>
--- resources/js/Pages/Trainees/Index.vue
+++ resources/js/Pages/Trainees/Index.vue
@@ -1,25 +1,33 @@
 <script setup>
-import {Head, router} from "@inertiajs/vue3";
+import {Head, router, useForm} from "@inertiajs/vue3";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import {Link} from "@inertiajs/vue3";
-import DashboardCard from "@/Components/DashboardCard.vue";
 import {ref} from "vue";
-import model from "@/Components/Model.vue";
-let isModalVisible = ref(false);
+import Drawer from "@/Components/Drawer.vue";
+import CloseBoxIcon from "vue-material-design-icons/CloseBox.vue"
 
 
-let showModal = () => {
-    isModalVisible = true;
-};
-let closeModal = () => {
-    isModalVisible = false;
+const isDrawerOpen = ref(false);
+const toggleDrawer = () => {
+    isDrawerOpen.value = !isDrawerOpen.value;
 };
 
-
-
-let props = defineProps({
+const closeDrawer = () => {
+    isDrawerOpen.value = false;
+};
+const props = defineProps({
     trainees: Object,
 });
+
+let form = useForm({
+    name: '',
+    email: '',
+    phone: '',
+});
+let submit = () => {
+    // console.log(form);
+    form.post(route('trainee.store'));
+};
 </script>
 
 <template>
@@ -39,13 +47,39 @@ let props = defineProps({
                     </div>
                 </div>
                 <div class="mt-2 mx-4 ">
-                    <div class="flex">
-                        <button class="  bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-xl">
-                            Add a Trainee
+                    <Drawer :is-open="isDrawerOpen" :speed="500" :max-width="'600px'" @close="closeDrawer">
+                        <button @click="closeDrawer">
+                            <close-box-icon/>
                         </button>
-                        <button @click="showModal" class="bg-blue-500 text-white px-4 py-2">Open Modal</button>
-                        <Model v-if="isModalVisible" @close="closeModal"/>
-                    </div>
+                        <form @submit.prevent="submit" class="ml-2 py-2 px-2 ">
+                            <!-- Left Side -->
+                            <div>
+                                <div class="mb-4">
+                                    <label for="name" class="block text-gray-600 text-sm font-medium mb-2">Name</label>
+                                    <input type="text" v-model="form.name" id="name" name="name" class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500" placeholder="John Doe" required>
+                                </div>
+
+                                <div class="mb-4">
+                                    <label for="phone" class="block text-gray-600 text-sm font-medium mb-2">Phone Number</label>
+                                    <input type="tel" v-model="form.phone" id="phone"  name="phone" class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500" placeholder="555-555-5555" required>
+                                </div>
+                                <div v-if="form.errors.phone" v-text="form.errors.phone" class="text-red-500 text-xs mt-1"></div>
+
+                                <div class="mb-4">
+                                    <label for="email" class="block text-gray-600 text-sm font-medium mb-2">Email</label>
+                                    <input type="email" id="email" v-model="form.email" name="email" class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500" placeholder="john.doe@example.com" required>
+                                </div>
+                                <div v-if="form.errors.phone" v-text="form.errors.phone" class="text-red-500 text-xs mt-1"></div>
+
+                                <div class="mt-0">
+                                    <button  type="submit" class="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue active:bg-blue-800">
+                                        Submit
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </Drawer>
+                    <button class="border bg-blue-300 mx-4 my-2 p-2 rounded-sm cursor-pointer hover:bg-blue-400 text-center" @click="toggleDrawer">Add Trainee</button>
                 </div>
             </div>
 
